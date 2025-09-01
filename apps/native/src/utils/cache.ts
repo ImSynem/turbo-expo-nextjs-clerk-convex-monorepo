224234600e8c95979dba93c768fdf30dceb568ee
Synem <imsynem@gmail.com>
2025-09-01 23:00:08 +0200

    Add expo-localization dependency and update app.json to include it in the native app configuration

--- apps/native/src/utils/cache.ts
+++ apps/native/src/utils/cache.ts
@@ -1,20 +1,15 @@
-import { TokenCache } from '@clerk/clerk-expo';
-import * as SecureStore from 'expo-secure-store';
-import { Platform } from 'react-native';
+import { TokenCache } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 const createTokenCache = (): TokenCache => {
   return {
     getToken: async (key: string) => {
       try {
         const item = await SecureStore.getItemAsync(key);
-        if (item) {
-          console.log(`${key} was used 🔐 \n`);
-        } else {
-          console.log('No values stored under key: ' + key);
-        }
         return item;
       } catch (error) {
-        console.error('secure store get item error: ', error);
+        console.error("secure store get item error: ", error);
         await SecureStore.deleteItemAsync(key);
         return null;
       }
@@ -27,4 +22,5 @@ const createTokenCache = (): TokenCache => {
 
 // SecureStore is not supported on the web
 // https://github.com/expo/expo/issues/7744#issuecomment-611093485
-export const tokenCache = Platform.OS !== 'web' ? createTokenCache() : undefined;
+export const tokenCache =
+  Platform.OS !== "web" ? createTokenCache() : undefined;
